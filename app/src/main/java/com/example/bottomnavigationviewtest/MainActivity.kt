b2ebe3fb5f272cadbf25b68c26f904bdff03f96b
jongc <jongchan159@gmail.com>
2024-07-10 03:31:15 +0900

    navigationbar color

--- app/src/main/java/com/example/bottomnavigationviewtest/MainActivity.kt
+++ app/src/main/java/com/example/bottomnavigationviewtest/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.bottomnavigationviewtest
 
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
 import androidx.navigation.NavOptions
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
@@ -30,6 +32,7 @@ class MainActivity : AppCompatActivity() {
         // Toolbar 설정
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
+        supportActionBar?.setBackgroundDrawable(ColorDrawable(ContextCompat.getColor(this, R.color.primaryColor)))
 
         val navView: BottomNavigationView = binding.navView
 
